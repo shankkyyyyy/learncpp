c0d421b8f96b0986a4f378591ca775b0303b931b
shankkkyyyyy <ramanshikillu@gmail.com>
2026-07-10 22:36:32 +0530

    Day-8 finished with making unique pointers in c++ from scratch

---
+++ cmake/CMakeFiles/Day8_run.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Day8_run.dir/home/gigu/learncpp/src/Day8/Day8.cc.o"
+  "CMakeFiles/Day8_run.dir/home/gigu/learncpp/src/Day8/Day8.cc.o.d"
+  "Day8_run"
+  "Day8_run.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Day8_run.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
