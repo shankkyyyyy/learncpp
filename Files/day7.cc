341081e9aa5723eb5b37d5024bca7dcdbddbcace
shankkkyyyyy <ramanshikillu@gmail.com>
2026-07-15 22:13:21 +0530

    Doc's testing

---
+++ Files/day7.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+
+typedef struct Animal
+{
+    int x; 
+    int y; 
+    void Print()
+        {
+            std::cout << "Hello, World!!" << std::endl; 
+            return;   
+        }
+} Animal; 
+
+typedef struct Bird
+{
+    int Name; 
+    int X;
+} Bird; 
+
+typedef struct Dog
+{
+    Animal x; 
+    Bird bird; 
+    int cs; 
+} Dog; 
+
+int PrintData(Animal* A)
+{
+    std::cout << A->x << "\n" << A->y << std::endl; 
+    return 1; 
+}
+
+int PrintData(Bird* C)
+{
+    std::cout << C->X << " <- X value" << std::endl; 
+    return 0;
+}
+
+int main()
+{
+    Dog A; 
+    Animal* B = (Animal*)&A; 
+    A.
+    Bird* c = (Bird*)&A;
+    PrintData(B); 
+    PrintData(c); 
+    return 0; 
+}
